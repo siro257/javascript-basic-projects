d3b55726974f49357f756b4bd79a6db94ef256a5
James Hwang <siro257@gmail.com>
2022-10-11 19:04:10 -0700

    completed 01

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,14 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  const n = getRandomNumber();
+  document.body.style.backgroundColor = colors[n];
+  color.textContent = colors[n];
+});
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * colors.length);
+};
